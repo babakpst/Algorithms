391aab408a23f2eec254f7f889b408870960bf41
Babak Poursartip <babakp@utexas.edu>
2018-08-05 17:02:03 -0500

    Add files via upload

---
+++ CourseI Divide and Conquer, Sorting and Searching, and Randomized Algorithms/Assignment I - Karatsuba/Karatsuba.cpp
@@ -0,0 +1,41 @@
+
+// Author: Babak Poursartip
+// Assignment I
+
+#include <string>
+#include <iostream>
+
+int Karatasuba(int num1, int num2)
+{
+
+if (num1 < 10 && num2 <10) return {num1*num2};
+else
+{
+Karatasuba()
+}
+
+
+
+
+
+}
+
+// the main code
+int main()
+{
+
+std::cout << std::endl <<" This is the Karatasuba algorithm."  << std::endl;
+
+// Integer mulitiplication
+//int number_1 = 123456789;
+//int number_2 = 123456789;
+
+int number_1 = 9;
+int number_2 = 9;
+
+std::cout << Karatasuba(number_1, number_1) << std::endl;
+std::cout<< std::endl  << " End of the code." << std::endl << std::endl;
+
+return 0;
+}
+
