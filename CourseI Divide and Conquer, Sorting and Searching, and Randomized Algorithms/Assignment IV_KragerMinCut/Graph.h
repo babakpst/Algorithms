8e972eded7f349b9a0ae419befcfc4686983c648
Babak Poursartip <babakp@utexas.edu>
2019-04-26 11:59:41 -0500

    Add files via upload

---
+++ CourseI Divide and Conquer, Sorting and Searching, and Randomized Algorithms/Assignment IV_KragerMinCut/Graph.h
@@ -0,0 +1,51 @@
+
+/*
+
+Developer: Babak Poursartip
+Assignment 4 of course I
+
+*/
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+
+class Graph{
+
+private:
+  int numV; // number of vertices
+  int numE; // number of edges
+
+  struct Edge{
+    int src, dest;
+    Edge();
+    Edge(int src, int dest);
+  };
+
+  std::vector<Edge> edges;
+
+
+public:
+
+  Graph();
+  Graph(int numV);
+  Graph(int numV, int numE);
+
+  int getNumV();
+  int getNumE();
+  
+  void read();
+  void addEdge(int src, int dest);
+  void printGraph();
+  //void printEdges(int numV);
+
+};
+
+#endif
--- CourseI Divide and Conquer, Sorting and Searching, and Randomized Algorithms/Assignment IV_KragerMinCut/main.cpp
+++ CourseI Divide and Conquer, Sorting and Searching, and Randomized Algorithms/Assignment IV_KragerMinCut/main.cpp
@@ -13,95 +13,31 @@ Date: April 23, 2019
 
 // libraries
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <string>
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-
-
-
-
-// ================================================================================================
-void printGraph(std::vector<int> adj[], int V) 
-{ 
-    std::cout << " number of vertices: " << V << std::endl;
-    for (int v = 0; v < V; ++v) 
-    { 
-        std::cout << "\n Adjacency list of vertex "
-             << v+1 << "\n head "; 
-        for (auto x : adj[v]) 
-           std::cout << "-> " << x+1; 
-        printf("\n"); 
-    } 
-} 
-
-// ================================================================================================
-int KargerMinCut(std::vector<int> mGraph, int& numV, int& numEdges){
-
-  int MinCut = 0;
-  int numReducedV = 0; // number of vertices after contraction
-  while ( numReducedV >2 ){
-
-
-
-  }
-
-
-
-  return MinCut;
-}
-
-
+#include "Graph.h"
 
 // ================================================================================================
 int main(){
 
 std::cout <<"\n Assignment IV: Karger minimum cut aglorthtm \n";
 
-// Define graph
+// Define graph -----------------------------------------------------------------------------------
 int numV = 8; // num of vertices in the graph
-std::vector<int> mGraph[numV];
+int iter = 200; // total number of running the algorithm to find the min cut
+int MinCut = numV; // minimum cut
 
+Graph mGraph(numV);
 
-// Reading the Graph from the file ----------------------------------------------------------------
-std::ifstream InputGraph("test.txt");
-std::string line;
+// Reading the Graph from the file
+mGraph.read();
+mGraph.printGraph();
 
-int counter = 0;
-int counterVertix; 
-int numEdges =0;
-int rootVertix; 
-while ( std::getline(InputGraph, line)){
-  counter++;
-  counterVertix = 0;
-  std::istringstream in(line);
-  int vertix = 0;
-  std::cout << " Reading vertix: " << counter << "\n";
-    while (in >> vertix){
-        counterVertix++;
-        if (counterVertix==1) rootVertix = vertix-1;
-        //std::cout << vertix << "\n";
-        if (counterVertix != 1) {
-          numEdges++;
-          mGraph[rootVertix].push_back(vertix-1);
-          std::cout << rootVertix << "   " << vertix << "\n";
-        }
-        }
-}
-
-InputGraph.close(); 
-numEdges = numEdges/2;
-printGraph(mGraph, 8);
 
 // contraction algorithm (Karger) -----------------------------------------------------------------
 
-srand(time(NULL));
+//srand(time(NULL));
 
-int MinCut = KargerMinCut(mGraph, numV, numEdges);
-std::cout << " \n Min number of cut is: " << MinCut << "\n";
+//int MinCut = KargerMinCut(mGraph, numV, numEdges);
+//std::cout << " \n Min number of cut is: " << MinCut << "\n";
 
 // Closing the code -------------------------------------------------------------------------------
 std::cout << "\n End of the code! \n";
