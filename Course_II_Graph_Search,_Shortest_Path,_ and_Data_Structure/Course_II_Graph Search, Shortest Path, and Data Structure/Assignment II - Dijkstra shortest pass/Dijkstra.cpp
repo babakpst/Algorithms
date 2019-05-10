c49af8bbab42459c3e033fa2304265fd98fbd4d8
babakpst <babakp@utexas.edu>
2019-05-10 10:09:44 -0500

    modifying the repository

---
+++ Course_II_Graph_Search,_Shortest_Path,_ and_Data_Structure/Course_II_Graph Search, Shortest Path, and Data Structure/Assignment II - Dijkstra shortest pass/Dijkstra.cpp
@@ -0,0 +1,43 @@
+
+/*
+===================================================================================================
+Developer: Babak Poursartip
+           The University of Texas at Austin
+
+Course II: Graph search, Shortest path, and Data Structure
+Assignment II: Dijkstra shortest path algorithm, without heap
+===================================================================================================
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "Graph.h"
+
+//=================================================================================================
+int main(){
+
+  std::cout << "\n Dijkstra shortest path algorithm for an undirected weighted graph \n";
+
+  // input ========================================================================================
+  std::string FileName;  // the name of the input file
+  FileName = "Dijkstra_Example.txt";
+  int numVertices = 9; // no. of vertices in the graph.
+
+  //FileName = "dijkstraData.txt";
+  //int numVertices = 200; // no. of vertices in the graph
+
+  // reading the graph ============================================================================
+  Graph Graph(FileName, numVertices);
+  
+  Graph.readGraph();
+  Graph.printGraph();
+  
+  // Dijkstra algo, finding the shortest path =====================================================
+
+
+ 
+  std::cout << "\n End of the code! \n";
+  return 0;
+}
