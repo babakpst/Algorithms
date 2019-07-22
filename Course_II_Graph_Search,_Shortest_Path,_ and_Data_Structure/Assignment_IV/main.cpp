4cefb59b6291c44a2715a7cb3265b0ff1bd5f11e
babakpst <babakpst@gmail.com>
2019-07-21 23:56:19 -0500

    Creating new folder for homework IV

---
+++ Course_II_Graph_Search,_Shortest_Path,_ and_Data_Structure/Assignment_IV/main.cpp
@@ -0,0 +1,29 @@
+
+/*
+===============================================================================
+Developer: Babak Poursartip
+           The University of Texas at Austin
+
+Course II:     Graph search, Shortest path, and Data Structure
+Assignment IV: The 2-Sum problem
+Data:           07/21/2019
+===============================================================================
+*/
+
+// libraries
+#include <iostream>
+#include <string>
+
+// objects
+
+int main() {
+
+  std::cout << " \n  --  2-Sum problem -- \n";
+
+  // int SizeOfStream = 20;                     // size of the stream of numbers
+  // std::string inputFileName = "example.txt"; // holds the input file name
+
+  std::cout << " The answer is: \n";
+  std::cout << " \n End of the code! \n";
+  return 0;
+}
